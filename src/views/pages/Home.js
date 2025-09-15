687c17155156338b8e6c1cffe18aa3bf68c9e6eb
ichiro <ChiTQHE171032@fpt.edu.vn>
2025-09-15 11:19:25 +0700

    First commit - React project

---
+++ src/views/pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { FaHandPeace } from "react-icons/fa"; // icon ngón tay 2
+
+function Home() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>
+        Hello Gr2 <FaHandPeace style={{ color: "blue", marginLeft: "10px" }} />
+      </h1>
+    </div>
+  );
+}
+
+export default Home;
