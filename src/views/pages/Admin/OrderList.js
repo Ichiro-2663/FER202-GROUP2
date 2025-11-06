21844d26fa2b5c00c14d27c835c639bd809634f9
thangtd632003 <144968411+thangtd632003@users.noreply.github.com>
2025-11-06 15:36:50 +0700

    Add OrderList component to display orders

---
+++ src/views/pages/Admin/OrderList.js
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from 'react';
+import { Table, Button } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import DashboardLayout from '../../components/DashboardLayout';
+
+export default function OrderList() {
+  const [orders, setOrders] = useState([]);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    axios.get('http://localhost:9999/orders')
+      .then(res => setOrders(res.data))
+      .catch(err => console.error(err));
+  }, []);
+
+  return (
+    <DashboardLayout className="p-4">
+      <h3> List of order</h3>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>User</th>
+            <th>Total</th>
+            <th>Address</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {orders.map(order => (
+            <tr key={order.id}>
+              <td>{order.id}</td>
+              <td>{order.user?.username}</td>
+              <td>{order.total.toLocaleString()}₫</td>
+              <td>{order.address}</td>
+              <td>
+                <Button variant="info" onClick={() => navigate(`/orders/${order.id}`)}>Detail</Button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </DashboardLayout>
+  );
+}
